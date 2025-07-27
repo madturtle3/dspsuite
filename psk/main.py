0106fbc5b7c39ededb3db35037c700533b709d60
Mason Braithwaite <mason@braith.net>
2025-07-26 21:54:18 -0600

    HELLO

--- psk/main.py
+++ psk/main.py
@@ -1,8 +1,8 @@
 # in comes the PSK!
 import numpy as np
 import numpy.typing as npt
+import scipy.signal as signal
 import matplotlib.pyplot as plt
-
 
 def getbits(bytearr: bytes) -> list[int]:
     """
@@ -29,7 +29,7 @@ def getbytes(bits: list[int], bytesize: int = 8) -> bytes:
 
 
 def modulate(
-    coded: npt.NDArray[np.complexfloating],
+    msg: npt.NDArray[np.complexfloating],
     Fs: int = 8000,
     Fb: float = 50,
     Fc: float = 700,
@@ -38,28 +38,43 @@ def modulate(
     Quite literally just turns complex numbers into a signal.
     """
     Spb: int = int(Fs / Fb)
-    IQ = np.repeat(coded, Spb)
+    IQ = np.repeat(msg, Spb)
+    # smooth it for less splatter
+    smooth = np.ones(10)
+    IQ = np.convolve(IQ,smooth,"same")
+    IQ = np.convolve(IQ,smooth,"same")
+    IQ = np.convolve(IQ,smooth,"same")
+    IQ = np.convolve(IQ,smooth,"same")
     # later on I may optionally convolve this to smooth out the baud boundaries
     # be more spectrally efficient
     time = np.arange(0, IQ.shape[0]) * 2 * np.pi * Fc / Fs
     # now do I*cos(x)+Q*sin(x)
     samples = IQ.real * np.cos(time) / 2 + IQ.imag * np.sin(time) / 2
+    plt.plot(samples)
+    plt.show()
     return samples
 
 
-if __name__ == "__main__":
-    # okay so imagine we have a message
-    msg = bytes(range(256))
-    # now we turn that into a bit array
-    bitwidth = 1
-    bits = list(getbytes(getbits(msg), bitwidth))
-    print(len(bits))
-    # now each element in bits is either 0,1,2, or 3.
-    # now we map each one to this bit space.
-    bitspace = np.linspace(-1, 1, 2**bitwidth)
-    comp_in = [ bitspace[bits[x]] + 1j*bitspace[bits[x+1]] for x in range(0,len(bits),2)]
+def demodulate(
+    samples: npt.NDArray[np.floating], Fs: int = 8000, Fc: float = 700
+) -> npt.NDArray[np.complexfloating]:
+    taps = signal.firls(81,[[0,Fc-50],[Fc-50,Fc+50],[Fc+50,Fs//2]],[[0,0],[1,1],[0,0]],fs=Fs)
+    filtered: npt.NDArray[np.floating] = np.convolve(samples,taps,"same")
+    time: npt.NDArray[np.floating] = (
+        np.arange(samples.shape[0], dtype=np.float32) * 2 * np.pi * Fc / Fs
+    )
+    iq: npt.NDArray[np.complexfloating] = np.cos(time) + 1j * np.sin(time)
+    iq *= filtered * 2
+    smoother = np.ones(int(Fs/Fc)) / int(Fs/Fc)
+    compout = np.convolve(iq,smoother,"same")
+    return compout
 
+if __name__ == "__main__":
+    comp_in: list[complex] = [1,1,1,-1,1,1]
     # how on earth do I code bytes into something interesting????
-    samples = modulate(np.array(comp_in), Fc=100)
+    samples = modulate(np.array(comp_in))
+    demod = demodulate(samples)
     plt.plot(samples)
+    plt.plot(demod.real)
+    plt.plot(demod.imag)
     plt.show()
--- tdd/main.py
+++ tdd/main.py
@@ -1,8 +1,6 @@
 import numpy as np
-import numpy.typing as npt
 import scipy.signal as signal
 import scipy.io.wavfile as wave
-import matplotlib.pyplot as plt
 
 LETTERS = [
     "\b",
@@ -85,8 +83,8 @@ def main():
         desired=[0, 0, 1, 1, 0, 0],
         fs=rate,
     )
-    samples = signal.lfilter(filt, 1, samples)
-    samples = signal.hilbert(samples)
+    samples = signal.lfilter(filt, 1, samples)  # type: ignore
+    samples = signal.hilbert(samples)  # type: ignore
     sig1 = samples[1:]
     sig2 = np.conjugate(samples[:-1])
     prod = sig1 * sig2
@@ -97,37 +95,32 @@ def main():
         (np.repeat(-1, SPB), np.repeat(0, SPB * 5), np.repeat(1, SPB * 2))
     )
     baud_detect = np.correlate(2 * binout - 1, baudot_template)
-    maxmima,properties = signal.find_peaks(baud_detect,2750,plateau_size=(None,None))
-    peaks_unrefined = properties["left_edges"]
+    _, properties = signal.find_peaks(baud_detect, 2750, plateau_size=(None, None))
+    peaks_unrefined = properties["left_edges"]  # type: ignore
     peaks = [peaks_unrefined[0]]
     diffs = np.diff(peaks_unrefined)
-    for i in range(1,peaks_unrefined.shape[0]):
-        if diffs[i-1]>SPB*5:
+    for i in range(1, peaks_unrefined.shape[0]):
+        if diffs[i - 1] > SPB * 5:
             peaks.append(peaks_unrefined[i])
 
     mode = "LETTERS"
-    plt.plot(freq)
-    plt.plot(baud_detect)
-    plt.plot(peaks_unrefined,baud_detect[peaks_unrefined],"x")
-    plt.plot(peaks,baud_detect[peaks],"x")
-    
-    plt.show()
     for i in peaks:
         idxs = [int(i + (1.5 + k) * SPB) for k in range(5)]
         val = 0
-        for j,digit in enumerate(binout[idxs]):
-            val |= digit<<j
-            #print(val)
+        for j, digit in enumerate(binout[idxs]):
+            val |= digit << j
+            # print(val)
         if FIGURES[val] == "LETTERS":
             mode = "LETTERS"
         elif LETTERS[val] == "FIGURES":
             mode = "FIGURES"
         else:
             if mode == "LETTERS":
-                print(LETTERS[val],end="",flush=True)
+                print(LETTERS[val], end="", flush=True)
             elif mode == "FIGURES":
-                print(FIGURES[val],end="",flush=True)
+                print(FIGURES[val], end="", flush=True)
     print()
+
 
 if __name__ == "__main__":
     main()
